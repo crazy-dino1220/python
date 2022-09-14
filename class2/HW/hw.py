881ee76ba3c6844a156a2f96fb9a7465cbe175de
name <changray1220@gmail.com>
2022-09-14 20:19:49 +0800

    剛剛沒用好

---
+++ class2/HW/hw.py
@@ -0,0 +1,22 @@
+'''
+請將下列變數做出正確的型態分類
+apple = 1000
+age = 12
+minor = False
+name = "David"
+weight = 64.5
+zip = "545"
+
+int整數  :zip
+bool布林數值 :minor
+str字串  :name
+float浮點數:weight
+
+example:
+ball = 100
+talk = 'hello'
+則要在型態列表中填入該變數
+int:ball
+str:talk
+
+'''

b91ae28bb4edd6b379c6bc2dca4b1335cbdfd637
name <changray1220@gmail.com>
2022-11-30 19:17:46 +0800

    ez

---
+++ class11/class/class.py
@@ -0,0 +1,11 @@
+b = []
+s = int(input("請輸入背包的大小:"))
+for i in range(s):
+    t = input("請輸入想裝的東西:")
+    b.append(t)
+    print(b)
+
+r = input("請輸入想刪除的東西:")
+while r in b:
+    b.remove(r)
+print(b)

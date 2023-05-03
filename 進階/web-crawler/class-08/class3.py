9ed0cc458d81dc9030319a5ae44a293d8f17ea20
name <changray1220@gmail.com>
2023-05-03 18:53:08 +0800

    ez

---
+++ 進階/web-crawler/class-08/class3.py
@@ -0,0 +1,48 @@
+import requests
+import os
+import sys
+import datetime
+import matplotlib.pyplot as plt
+
+os.chdir(sys.path[0])
+
+api_key = "892da2f13edf3c7f382637760e72d224"
+
+base_url = "https://api.openweathermap.org/data/2.5/onecall?"
+lon = "121.5319"
+lat = "25.0418"
+exclude = "minutely,hourly"
+units = "metric"
+lang = "zh_tw"
+
+send_url = base_url
+send_url += "lat=" + lat
+send_url += "&lon=" + lon
+send_url += "&exclude=" + exclude
+send_url += "&appid=" + api_key
+send_url += "&units=" + units
+send_url += "&lang=" + lang
+print(send_url)
+
+responese = requests.get(send_url)
+info = responese.json()
+
+listX = []
+listY = []
+
+if "daily" in info.keys():
+    for i in range(7):
+        temp = info["daily"][i]["temp"]["day"]
+        time = datetime.datetime.fromtimestamp(
+            info["daily"][i]["dt"]).strftime("%m/%d")
+        print(f"{time}的溫度是{temp}度")
+        listX.append(time)
+        listY.append(temp)
+else:
+    print("#$%^#@$&&%#")
+fig, ax = plt.subplots()
+ax.plot(listX, listY)
+ax.set_xlabel('time')
+ax.set_ylabel('temp')
+ax.set_title('wether')
+plt.show()
